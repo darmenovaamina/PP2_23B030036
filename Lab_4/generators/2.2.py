c3330461002b06e48413fb6d28fa2b900bdff5f5
darmenovaamina <aminadarmenova@icloud.com>
2025-02-18 11:47:03 +0500

    adding lab4

---
+++ Lab_4/generators/2.2.py
@@ -0,0 +1,3 @@
+a=int(input())
+s=[x for x in range(a) if x%2==0]
+print(s)
---
+++ Lab_4/math/1.py
@@ -0,0 +1,6 @@
+
+from math import pi
+n=int(input())
+def to_radian(int):
+    return (int*pi)/180
+print(round(to_radian(n), 6))
